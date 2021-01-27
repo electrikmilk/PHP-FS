7afc08fa3ea16056d1d5c4d9eab4e6869b02aa67
electrikmilk <brandonjordan124@gmail.com>
2021-01-27 16:37:05 -0500

    Corrected credit

--- src/Files.php
+++ src/Files.php
@@ -1,6 +1,6 @@
 <?php
 /*
- * Created by @electrikmilk, 01-21-2020
+ * Created by @electrikmilk, 01-21-2021
  * This file is subject to the terms and conditions defined in
  * file 'LICENSE', which is part of this source code package.
 */
